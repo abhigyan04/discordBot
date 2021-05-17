aa27526d32b814aa61a88ebaf70bdd7dc72cfde4
Abhigyan Gandhi <abhigyangandhi04@gmail.com>
2021-05-17 15:05:39 +0530

    Update hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'hi',
     description: 'This is a hi command',
     execute(message, args){
-        if(message.member.roles.cache.has('759194564325670912')){
+        if(message.member.roles.cache.has('Enter the role ID here')){
             message.channel.send('Hey there!');
         }
         else{
