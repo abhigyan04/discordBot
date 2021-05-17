a8288cf6b3644352ef1dd57d5f509d26f42ac905
Abhigyan Gandhi <abhigyangandhi04@gmail.com>
2021-05-17 15:07:55 +0530

    Update removeperm.js

--- commands/removeperm.js
+++ commands/removeperm.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'removeperm',
     description: 'This is a removeperm command',
     execute(message, args){
-        if(message.member.roles.cache.has('759194564325670912')){
-            message.member.roles.remove('759194564325670912');
+        if(message.member.roles.cache.has('Enter the role ID here')){
+            message.member.roles.remove('Enter the role ID here');
             message.channel.send("You no longer have the permission to use this bot");
         }
     }
