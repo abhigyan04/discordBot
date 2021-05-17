4cf9ef9fa914c4b452446492d6eacd5985662cef
Abhigyan Gandhi <abhigyangandhi04@gmail.com>
2021-05-17 15:04:19 +0530

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'commands',
     description: 'This is a commands command',
     execute(message, args){
-        if(message.member.roles.cache.has('759194564325670912')){
+        if(message.member.roles.cache.has('Enter the role ID here')){
             message.channel.send('Use these commands to moderate your games!');
             message.channel.send('$muteall : To mute all members in the vc');
             message.channel.send('$unmuteall : To unmunte all members in the vc');
