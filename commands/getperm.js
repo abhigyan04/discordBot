302eed92b92159cabe76e51a2a27e8e8ed9abb01
Abhigyan Gandhi <abhigyangandhi04@gmail.com>
2021-05-17 15:05:20 +0530

    Update getperm.js

--- commands/getperm.js
+++ commands/getperm.js
@@ -2,14 +2,14 @@ module.exports = {
     name: 'getperm',
     description: 'This is a getperm command',
     execute(message, args){
-        if(message.member.roles.cache.has('759194564325670912')){
+        if(message.member.roles.cache.has('Enter the role ID here')){
             message.channel.send('You already have the permission to use this bot');
         }
-        else if(!message.member.roles.cache.has('760723760751444008')){
+        else if(!message.member.roles.cache.has('Enter the admin role ID here')){
             message.channel.send('You are not an admin');
         }
         else{
-            message.member.roles.add('759194564325670912');
+            message.member.roles.add('Enter the role ID here');
             message.channel.send('You now have the permission to use this bot :)');
             message.channel.send('To remove the permission type $removePerm');
         }
